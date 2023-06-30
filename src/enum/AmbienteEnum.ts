57b170df6e89b77c74621d58570f0ec2d4666fc4
rafael <r_s_carvalho@yahoo.com.br>
2023-06-30 10:37:28 -0300

    Atualização da URL de Produção

--- src/enum/AmbienteEnum.ts
+++ src/enum/AmbienteEnum.ts
@@ -2,5 +2,6 @@
  * Lista de servidores por ambiente.
  */
 export enum AmbienteEnum {
-    HOMOLOGACAO = 'https://hom-nfoe.estaleiro.serpro.gov.br/API'
+    HOMOLOGACAO = "https://hom-nfoe.estaleiro.serpro.gov.br/API",
+    PRODUCAO = "https://nfeouro.rfb.gov.br/API"
 }
